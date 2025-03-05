52470b16ba5292aa2c7b3fd51e03ae30d1483f0e
BuiGiang <buiducgiang>
2025-03-05 11:28:24 +0700

    config flutter base bloc

---
+++ lib/core/core.dart
@@ -0,0 +1,16 @@
+export 'bloc/base_bloc.dart';
+export 'bloc/base_list_bloc.dart';
+export 'bloc/observer.dart';
+export 'bloc/state_wrapper.dart';
+export 'bloc/view_state.dart';
+export 'bloc/wrapper.dart';
+export 'cache/cache_helper.dart';
+export 'cache/cache_helper_impl.dart';
+export 'navigation/navigate_event.dart';
+export 'progress/progress_event.dart';
+export 'toast/toast_event.dart';
+export 'widget/space_widget.dart';
+export 'page/base_page.dart';
+export 'page/base_list_page.dart';
+export 'extension/context_ext.dart';
+export 'extension/string_ext.dart';
